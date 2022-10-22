795a126bde4482e795dc39ba15b07995ff992547
Weasley <1432689025@qq.com>
2022-10-22 17:00:55 +0800

    update

--- src/test/java/cn/alphahub/code/generator/CodeGeneratorApplicationTests.java
+++ src/test/java/cn/alphahub/code/generator/CodeGeneratorApplicationTests.java
@@ -1,13 +1,16 @@
 package cn.alphahub.code.generator;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class CodeGeneratorApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("你好！");
     }
 
 }
